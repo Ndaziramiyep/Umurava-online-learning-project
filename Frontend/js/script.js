eb9c37b7822599b9cf1ebb88ac33387508b0a5e8
Ndaziramiyep <patrickofficial30@gmail.com>
2024-07-14 08:09:18 +0200

    Adding js to make navlinks to appear after clicking menubar fo small screens

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -6,3 +6,14 @@ const navs = document.querySelectorAll("a");
          nav.classList.add("active")
     });
 });
+
+// -------------------making #mobile (menu) clickable ----------
+
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+
+if (bar){
+    bar.addEventListener("click", () => {
+        nav.classList.add('active');
+    })
+}
