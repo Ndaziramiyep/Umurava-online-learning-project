72378b213d8bc12a7a2b8cb657f8d8227e20a430
Ndaziramiyep <patrickofficial30@gmail.com>
2025-01-14 22:59:34 +0200

    Updating codes

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('accounts/profile/', views.profile, name="profile"),
-    path('accounts/update-profile/', views.update_profile, name="update-profile"),
-    path('accounts/save-profile/', views.save_profile, name="save-profile"),
-]
